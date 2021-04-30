98b957d827da9c5050ce736d267e94c26c59484a
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-04-30 15:11:16 -0300

    item view and style

--- index.py
+++ index.py
@@ -42,8 +42,31 @@ def application(environ, start_response):
         buffer = buffer + '</table>'
         buffer = buffer + '</body>'
         buffer = buffer + '</html>'
+    elif environ['PATH_INFO'][:10] == '/question/':
+        id = int(environ['PATH_INFO'][10:])
+        con = psycopg2.connect(host=config.host, database=config.database, user=config.user, password=config.password)
+        sql = 'select * from interview_questions where interview_question_id = %(interview_question_id)s'
+        cur = con.cursor()
+        cur.execute(sql,{'interview_question_id':id})
+        buffer = buffer + '<html>'
+        buffer = buffer + '<head>'
+        buffer = buffer + '<link rel="stylesheet" href="../p17data/ItemStyle.css">'
+        buffer = buffer + '</head>'
+        buffer = buffer + '<body>'
+        buffer = buffer + '<dl>'
+        recset = cur.fetchall()
+        for rec in recset:
+            buffer = buffer + '<dt>id</dt><dd>' + str(rec[0]) + '</dd>'
+            buffer = buffer + '<dt>name</dt><dd>' + str(rec[1]) + '</dd>'
+            buffer = buffer + '<dt>question</dt><dd>' + str(rec[2]) + '</dd>'
+            buffer = buffer + '<dt>answer</dt><dd>' + str(rec[3]) + '</dd>'
+        buffer = buffer + '</dl>'
+        buffer = buffer + '</body>'
+        buffer = buffer + '</html>'
     elif environ['PATH_INFO'] == '/p17data/TableList.css':
         buffer = open('p17data/TableList.css','r').read()
+    elif environ['PATH_INFO'] == '/p17data/ItemStyle.css':
+        buffer = open('p17data/ItemStyle.css','r').read()
     else:
         buffer = str(environ['PATH_INFO'])
     return [str(buffer).encode('ascii','ignore')]
